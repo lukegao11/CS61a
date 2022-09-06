6f62e4cb9ff3c56aa75163e0c0b4ead24df9da7d
Guanqun Gao <99423162+lukegao11@users.noreply.github.com>
2022-09-06 11:09:04 -0400

    Update hw02.py

--- homework/hw02/hw02.py
+++ homework/hw02/hw02.py
@@ -159,9 +159,12 @@ def two(f):
 
 three = successor(two)
 
+
+## couldn't 
+## source: https://github.com/PKUFlyingPig/CS61A/blob/master/hws/hw02/hw02.py
+
 def church_to_int(n):
     """Convert the Church numeral n to a Python integer.
-
     >>> church_to_int(zero)
     0
     >>> church_to_int(one)
@@ -172,18 +175,24 @@ def church_to_int(n):
     3
     """
     "*** YOUR CODE HERE ***"
+    def add1(x):
+        return x+1
+    return n(add1)(0)
 
 def add_church(m, n):
     """Return the Church numeral for m + n, for Church numerals m and n.
-
     >>> church_to_int(add_church(two, three))
     5
     """
     "*** YOUR CODE HERE ***"
+    int_n = church_to_int(n)
+    for i in range(int_n):
+        m = successor(m)
+    return m
+
 
 def mul_church(m, n):
     """Return the Church numeral for m * n, for Church numerals m and n.
-
     >>> four = successor(three)
     >>> church_to_int(mul_church(two, three))
     6
@@ -191,14 +200,23 @@ def mul_church(m, n):
     12
     """
     "*** YOUR CODE HERE ***"
+    ans = zero
+    int_n = church_to_int(n)
+    for i in range(int_n):
+        ans = add_church(ans, m)
+    return ans
 
 def pow_church(m, n):
     """Return the Church numeral m ** n, for Church numerals m and n.
-
     >>> church_to_int(pow_church(two, three))
     8
     >>> church_to_int(pow_church(three, two))
     9
     """
     "*** YOUR CODE HERE ***"
+    ans = one
+    int_n = church_to_int(n)
+    for i in range(int_n):
+        ans = mul_church(ans, m)
+    return ans
 
